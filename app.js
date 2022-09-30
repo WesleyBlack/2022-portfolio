f70483234a0ee27d7254229ae7aa7a96faf097f3
WesleyBlack <wesleyblackaw@gmail.com>
2022-09-30 03:47:00 -0300

    Update app.js
    Accessibility improvements

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ let root = document.querySelector(':root');
 
 function dimensions() {
     
+    function test(){
+    var ratio = window.devicePixelRatio;            
+    settings = ratio-window.innerHeight; 
+    console.log("NEW TITLE BAR HEIGHT: " + settings);
+};
+    test()
+    
     var scale;
     
     if (window.innerWidth >= window.innerHeight) {
@@ -26,6 +33,27 @@ function dimensions() {
 }
 
 
+// Rescaling fix for mobile safari when changing orientation
+const mediaQuery = window.matchMedia('(orientation:landscape)')
+
+function handleOrientationChange(e) {
+    if (e.matches) {
+       function loop(i) {
+           if (i < 0) return;
+           setTimeout(function () {
+               dimensions();
+               loop(--i);
+           }, 1);
+       }
+       loop(100);
+    } else {
+       setTimeout(dimensions, 340);
+   }
+ }
+
+mediaQuery.addListener(handleOrientationChange)
+
+
 // Carousel
 var slidePosition = 1;
 var slide = document.getElementsByClassName("card");
@@ -36,6 +64,7 @@ function firstSlide() {
     for (let i = slidePosition; i > 2; i--) {
         slide[slidePosition=i-2].style.transitionDuration = "0s";
         slide[slidePosition=i-2].style.transform = "rotateY(+180deg)";
+        setTimeout(function(){ slide[slidePosition=i-2].style.visibility = "hidden"; }, 1000);
     }
     slideShow(slidePosition = 1);
 }
@@ -45,9 +74,11 @@ function plusSlide(n) {
       slide[slidePosition].style.transitionDuration = "1s";
       slide[slidePosition-1].style.transform = "rotateY(-180deg)";
       slide[slidePosition-1].style.pointerEvents = "none";
+      setTimeout(function(){ slide[slidePosition-2].style.visibility = "hidden"; }, 1000);
   } else {
       slide[slidePosition-1].style.transform = "rotateY(+180deg)";
       slide[slidePosition-1].style.pointerEvents = "none";
+      setTimeout(function(){ slide[slidePosition].style.visibility = "hidden"; }, 1000);
   }
   slideShow(slidePosition += n);
 }
@@ -66,4 +97,5 @@ function slideShow(n) {
     }
     slide[slidePosition-1].style.transform = "rotateY(0deg)";
     slide[slidePosition-1].style.pointerEvents = "auto";
+    slide[slidePosition-1].style.visibility = "visible";
 }
